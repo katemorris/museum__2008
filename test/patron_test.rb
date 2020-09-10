d4964aee011451fe87cfee1ea61035a4d885881b
Kate Tester <katemorris@gmail.com>
2020-09-10 09:11:06 -0600

    Adds required tests for Patron, no class yet.

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -3,23 +3,21 @@ require 'minitest/pride'
 require './lib/patron'
 
 class PatronTest < Minitest::Test
+  def setup
+    @patron_1 = Patron.new("Bob", 20)
+  end
 
+  def test_it_has_attributes
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+    assert_empty @patron_1.interests
+
+  end
+
+  def test_patron_can_add_interests
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
+  end
 end
-pry(main)> patron_1 = Patron.new("Bob", 20)
-# => #<Patron:0x00007fcb13b5c7d8...>
-
-pry(main)> patron_1.name
-# => "Bob"
-
-pry(main)> patron_1.spending_money
-# => 20
-
-pry(main)> patron_1.interests
-# => []
-
-pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-
-pry(main)> patron_1.add_interest("Gems and Minerals")
-
-pry(main)> patron_1.interests
-# => ["Dead Sea Scrolls", "Gems and Minerals"]
