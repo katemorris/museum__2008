1d519007efc873bb1454be58888c7e74e43c9b30
Kate Tester <katemorris@gmail.com>
2020-09-10 09:59:41 -0600

    Adds test for showing exhibits and interested patrons.

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -60,18 +60,36 @@ class MuseumTest < Minitest::Test
     @dmns.admit(@patron_2)
     @dmns.admit(@patron_3)
 
-    assert_equal [@patron_1, @patron_2, @patron_3] @dmns.patrons
+    assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
+  end
+
+  def test_it_can_return_patrons_by_exhibit_interest
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
+    @dmns.admit(patron_3)
+
+    expected = {
+      @gems_and_minerals => [patron_1],
+      @dead_sea_scrolls => [patron_1, patron_2, patron_3],
+      @imax => []
+    }
+    @dmns.patrons_by_exhibit_interest
   end
 end
-#
-# pry(main)> dmns.patrons_by_exhibit_interest
-# # =>
-# # {
-# #   #<Exhibit:0x00007fb202238618...> => [#<Patron:0x00007fb2011455b8...>],
-# #   #<Exhibit:0x00007fb202248748...> => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x00007fb20227f8b0...>, #<Patron:0x6666fb20114megan...>],
-# #   #<Exhibit:0x00007fb20225f8d0...> => []
-# # }
-#
 # pry(main)> dmns.ticket_lottery_contestants(dead_sea_scrolls)
 # # => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x6666fb20114megan...>]
 #
