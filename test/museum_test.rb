0c5196a0f82f1e7e807b8099c442dcd1dc168a7b
Kate Tester <katemorris@gmail.com>
2020-09-10 09:47:51 -0600

    Adds admitting patrons test and extra patron to setup.

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -19,6 +19,8 @@ class MuseumTest < Minitest::Test
     @patron_2 = Patron.new("Sally", 20)
     @patron_2.add_interest("IMAX")
 
+    @patron_3 = Patron.new("Johnny", 5)
+    @patron_3.add_interest("Dead Sea Scrolls")
   end
 
   def test_it_has_attributes
@@ -42,9 +44,51 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@imax)
 
     p1_expected = [@gems_and_minerals, @dead_sea_scrolls]
-    p2_expected = [@imax]
 
     assert_equal p1_expected, @dmns.recommend_exhibits(@patron_1)
-    assert_equal p2_expected, @dmns.recommend_exhibits(@patron_2)
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+  end
+
+  def test_it_can_admit_patrons
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_empty @dmns.patrons
+
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    assert_equal [@patron_1, @patron_2, @patron_3] @dmns.patrons
   end
 end
+#
+# pry(main)> dmns.patrons_by_exhibit_interest
+# # =>
+# # {
+# #   #<Exhibit:0x00007fb202238618...> => [#<Patron:0x00007fb2011455b8...>],
+# #   #<Exhibit:0x00007fb202248748...> => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x00007fb20227f8b0...>, #<Patron:0x6666fb20114megan...>],
+# #   #<Exhibit:0x00007fb20225f8d0...> => []
+# # }
+#
+# pry(main)> dmns.ticket_lottery_contestants(dead_sea_scrolls)
+# # => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x6666fb20114megan...>]
+#
+# pry(main)> dmns.draw_lottery_winner(dead_sea_scrolls)
+# # => "Johnny" or "Bob" can be returned here. Fun!
+#
+# pry(main)> dmns.draw_lottery_winner(gems_and_minerals)
+# # => nil
+#
+# #If no contestants are elgible for the lottery, nil is returned.
+#
+# pry(main)> dmns.announce_lottery_winner(imax)
+# # => "Bob has won the IMAX edhibit lottery"
+#
+# # The above string should match exactly, you will need to stub the return of `draw_lottery_winner` as the above method should depend on the return value of `draw_lottery_winner`.
+#
+# pry(main)> dmns.announce_lottery_winner(gems_and_minerals)
+# # => "No winners for this lottery"
+#
+# # If there are no contestants, there are no winners.
