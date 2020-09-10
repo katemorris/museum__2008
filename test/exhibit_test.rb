7d6babb3d7c724ed5ed9763e9109263d1b1ddac9
Kate Tester <katemorris@gmail.com>
2020-09-10 09:13:20 -0600

    Fixes wrong word in name of exhibit in test.

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -8,7 +8,7 @@ class ExhibitTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "Gems and Materials", @exhibit.name
+    assert_equal "Gems and Minerals", @exhibit.name
     assert_equal 0, @exhibit.cost
   end
 end
