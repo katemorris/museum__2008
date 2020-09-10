b292c3ca8c1f7537e9843f4ad0e39748b1896e9f
Kate Tester <katemorris@gmail.com>
2020-09-10 09:08:32 -0600

    Adds required tests for Exhibit, no class yet.

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -3,13 +3,12 @@ require 'minitest/pride'
 require './lib/exhibit'
 
 class ExhibitTest < Minitest::Test
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
 
+  def test_it_has_attributes
+    assert_equal "Gems and Materials", @exhibit.name
+    assert_equal 0, @exhibit.cost
+  end
 end
-pry(main)> exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-# => #<Exhibit:0x00007fcb13bd22d0...>
-
-pry(main)> exhibit.name
-# => "Gems and Minerals"
-
-pry(main)> exhibit.cost
-# => 0
