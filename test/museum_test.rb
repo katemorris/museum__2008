6a928b8b85ae1403951e5e05576ffbcc8e51adfb
Kate Tester <katemorris@gmail.com>
2020-09-10 09:27:24 -0600

    Museum test file complete to spec.

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -7,41 +7,40 @@ require './lib/exhibit'
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("IMAX")
 
   end
 
   def test_it_has_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
+  end
+
+  def test_it_can_add_exhibits
     assert_empty @dmns.exhibits
+
+    @dmns.add_exhibit(gems_and_minerals)
+    @dmns.add_exhibit(dead_sea_scrolls)
+    @dmns.add_exhibit(imax)
+
+    expected = [@gems_and_minerals, @dead_sea_scrolls, @imax]
+    assert_equal expected, @dmns.exhibits
+  end
+
+  def test_it_can_recommend_exhibits_to_patrons
+    p1_expected = expected = [@gems_and_minerals, @dead_sea_scrolls]
+    p2_expected = expected = [@imax]
+    
+    assert_equal p1_expected, @dmns.recommend_exhibits(patron_1)
+    assert_equal p2_expected, @dmns.recommend_exhibits(patron_2)
   end
 end
-
-dmns.add_exhibit(gems_and_minerals)
-
-dmns.add_exhibit(dead_sea_scrolls)
-
-dmns.add_exhibit(imax)
-
-dmns.exhibits
-# => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>, #<Exhibit:0x00007fb400acc590...>]
-
-patron_1 = Patron.new("Bob", 20)
-# => #<Patron:0x00007fb400a51cc8...>
-
-patron_1.add_interest("Dead Sea Scrolls")
-
-patron_1.add_interest("Gems and Minerals")
-
-patron_2 = Patron.new("Sally", 20)
-# => #<Patron:0x00007fb400036338...>
-
-patron_2.add_interest("IMAX")
-
-dmns.recommend_exhibits(patron_1)
-# => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>]
-
-dmns.recommend_exhibits(patron_2)
-# => [#<Exhibit:0x00007fb400acc590...>]
